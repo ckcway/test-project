925a91dbe815f3246289c3ae1f1de19953dba415
chencheng <ckcway@163.com>
2018-06-25 19:28:01 +0800

    add log print in addEmployee() nethod for oa-web

--- oa-web/src/main/java/com/zhss/oa/web/controller/WebController.java
+++ oa-web/src/main/java/com/zhss/oa/web/controller/WebController.java
@@ -27,6 +27,7 @@ public class WebController {
 	public String addEmployee(Employee employee) {
 		try {
 			employeeService.addEmployee(employee);
+			System.out.println("add employee ....");
 		} catch (Exception e) {
 			e.printStackTrace(); 
 			return "error";
